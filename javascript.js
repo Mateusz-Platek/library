1027d80dbfbf218b5812afc8550a2059f83f3a48
Mateusz-Platek <m.platek1705@gmail.com>
2022-09-21 12:14:01 +0200

    Remove old function

--- javascript.js
+++ javascript.js
@@ -1,12 +1,5 @@
 let books = [];
 
-// function Book(title, author, pages, read) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-// }
-
 class Book {
     constructor(title, author, pages, read) {
         this.title = title;
