bad694889c01465515354a116083d53ad63358cd
Mateusz-Platek <m.platek1705@gmail.com>
2022-09-21 12:10:31 +0200

    Refactor book function to class

--- javascript.js
+++ javascript.js
@@ -1,10 +1,19 @@
 let books = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+// }
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 let addBook = document.querySelector('.addBook');
